6b1a92a0869cdc856cbb3023f4ccd746cc0aace4
drewgrif <drew@drewgriffin.com>
2022-05-18 13:32:26 -0400

    Update autostart.sh

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -2,7 +2,9 @@
 # Autostart applications
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-nitrogen --restore; sleep 1; picom -b &
+# nitrogen --restore; sleep 1; 
+feh --bg-fill ~/.config/backgrounds/wallhaven-y8o51x_1920x1080.png &
+picom -b &
 numlockx on &
 nm-applet &
 volumeicon &
