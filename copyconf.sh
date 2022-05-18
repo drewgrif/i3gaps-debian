a405124ebc418b3d398e96078cf9f12ca07fce13
drewgrif <drew@drewgriffin.com>
2022-05-18 14:38:37 -0400

    Update copyconf.sh

--- copyconf.sh
+++ copyconf.sh
@@ -1,6 +1,13 @@
 ####
 # This script is used to copy my config files over for use on reboot of installation.
 
+# Using bumblebee-status instead of polybar or i3status (installed for initial login)
+# https://bumblebee-status.readthedocs.io/en/main/index.html
+pip install --user bumblebee-status
+
+# Package needed for bumblebee-status if using apt module - currently configured is i3gaps config file.
+sudo apt install -y aptitude
+
 cp -r .config/backgrounds/ ~/.config
 cp -r .config/bumblebee-status/ ~/.config
 cp -r .config/i3/ ~/.config
