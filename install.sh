804ff276370781c6858eedf683f2d707908f4108
drewgrif <drew@drewgriffin.com>
2022-05-18 13:20:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,10 +53,10 @@ sudo systemctl enable cups
 sudo apt install -y firefox-esr 
 
 # Desktop background browser/handler 
-# feh -g 3440x1440 -d -S filename /path/to/directory 
+# feh --bg-fill /path/to/directory 
 # example if you want to use in autostart.sh for i3-gaps
-# sudo apt install -y feh
-sudo apt install -y nitrogen 
+# sudo apt install -y nitrogen 
+sudo apt install -y feh
 
 # Required packages for i3-gaps installation
 sudo apt install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev i3status libpam0g-dev libxcb-xkb-dev
